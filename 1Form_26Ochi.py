22f4973afb96591932f0dfa30482c8a1720d690d
Nattapong26 <nattapongthasringam@gmail.com>
2024-12-04 16:07:46 +0700

    Add files via upload

---
+++ 1Form_26Ochi.py
@@ -0,0 +1,13 @@
+print("Welcome to ROV")
+
+print("\n\nสามารถกรอกข้อมูลที่นี้ได้เลยครับ")
+name = input("กรอกชื่อ")
+password = input("กรอกรหัส")
+travel = input("กรอกชื่อสถานที่")
+time = input("กรอกเวลา")
+
+print("\n\nยินดีต้อนรับ" , name )
+print("\n\npassword" , password)
+print("เลือกสถานที่" , travel)
+print("เวลาที่ต้องการ" , time)
+print("ขอบคัณที่ใช้บริการ") 
